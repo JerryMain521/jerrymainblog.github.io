a5091e4ebe5822c2fb0ae271a329b3e3f391a6cc
JerryMain <16806246+jerrymain521@user.noreply.gitee.com>
2026-04-06 00:44:46 +0800

    Replace with new site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  markdown: {
+    // 代码高亮 Astro 默认用 Shiki，这里可以指定主题
+    shikiConfig: {
+      theme: 'github-dark',  // 或 'github-light'、'one-dark-pro' 等
+    },
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  }
+});
